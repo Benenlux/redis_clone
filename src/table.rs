e1942f422071a7cb7c5e245bfd04cb026645e45a
Benenlux <simvee2000@gmail.com>
2026-02-24 20:51:33 +0100

    test(table): added set test
    Added a simple set test

--- src/table.rs
+++ src/table.rs
@@ -7,4 +7,15 @@ pub struct Table {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_set_string() {
+        let test_string: Vec<String> = vec!["Currency".to_string(), "Euro".to_string()];
+        let mut table = Table::new();
+        let result = table.set(test_string[0].clone(), test_string[1].clone());
+
+        assert_eq!("OK", result);
+    }
+}
