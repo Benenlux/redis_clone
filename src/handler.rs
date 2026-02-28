3f9e2183c2b639ac581f3e8df194e223b2e1bcd1
Benenlux <simvee2000@gmail.com>
2026-02-28 15:44:51 +0100

    feat: implemented get & set functionality
    Implemented the get and set function to handle_request

--- src/handler.rs
+++ src/handler.rs
@@ -1,3 +1,6 @@
+use std::{str::FromStr, sync::Arc};
+
+use crate::table::Table;
 
 #[derive(Debug, PartialEq)]
 pub enum CommandTypes {
@@ -15,6 +18,29 @@ impl FromStr for CommandTypes {
             "SET" => Ok(CommandTypes::Set),
             "GET" => Ok(CommandTypes::Get),
             _ => Err(CommandParseError),
+        }
+    }
+}
+
+pub fn handle_request(request: Vec<String>, table: &Arc<Table>) -> String {
+    let mut req_iter = request.into_iter();
+    let req_command = match req_iter.next() {
+        Some(val) => val,
+        None => return String::from("+Error\r\n"),
+    };
+    let command = match req_command.parse::<CommandTypes>() {
+        Ok(command) => command,
+        Err(_) => return String::from("+Error\r\n"),
+    };
+    match command {
+        CommandTypes::Get => {
+            let key = req_iter.next().unwrap_or_default();
+            table.get(key)
+        }
+        CommandTypes::Set => {
+            let key = req_iter.next().unwrap_or_default();
+            let val = req_iter.next().unwrap_or_default();
+            table.set(key, val)
         }
     }
 }
